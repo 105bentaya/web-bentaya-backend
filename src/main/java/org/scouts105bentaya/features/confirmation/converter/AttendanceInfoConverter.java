bf84498fdf6bc8d72743d0e67655255a6a421072
dk96hz <95834129+dk96hz@users.noreply.github.com>
2025-06-17 03:04:43 +0100

    Attendance info improve

--- src/main/java/org/scouts105bentaya/features/confirmation/converter/AttendanceInfoConverter.java
+++ src/main/java/org/scouts105bentaya/features/confirmation/converter/AttendanceInfoConverter.java
@@ -2,9 +2,14 @@ package org.scouts105bentaya.features.confirmation.converter;
 
 import org.scouts105bentaya.features.confirmation.Confirmation;
 import org.scouts105bentaya.features.confirmation.dto.AttendanceInfoDto;
+import org.scouts105bentaya.features.scout.entity.MedicalData;
+import org.scouts105bentaya.features.scout.entity.Scout;
 import org.scouts105bentaya.shared.GenericConstants;
 import org.scouts105bentaya.shared.GenericConverter;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class AttendanceInfoConverter extends GenericConverter<Confirmation, AttendanceInfoDto> {
@@ -22,7 +27,31 @@ public class AttendanceInfoConverter extends GenericConverter<Confirmation, Atte
             entity.getAttending(),
             entity.getPayed(),
             entity.getText(),
-            entity.getScout().getMedicalData().getMedicalDiagnoses()
+            medicalDataFromScout(entity.getScout())
         );
+    }
+
+    private static String medicalDataFromScout(Scout scout) {
+        MedicalData medicalData = scout.getMedicalData();
+        List<String> medicalDataString = new ArrayList<>();
+        if (medicalData.getFoodAllergies() != null) {
+            medicalDataString.add("Alergias: " + medicalData.getFoodAllergies());
+        }
+        if (medicalData.getFoodIntolerances() != null) {
+            medicalDataString.add("Intolerancias: " + medicalData.getFoodIntolerances());
+        }
+        if (medicalData.getFoodDiet() != null) {
+            medicalDataString.add("Dietas: " + medicalData.getFoodDiet());
+        }
+        if (medicalData.getFoodProblems() != null) {
+            medicalDataString.add("Problemas alimentarios: " + medicalData.getFoodProblems());
+        }
+        if (medicalData.getFoodMedication() != null) {
+            medicalDataString.add("Medicación alimentaria: " + medicalData.getFoodMedication());
+        }
+        if (medicalDataString.isEmpty()) {
+            return null;
+        }
+        return String.join(" - ", medicalDataString);
     }
 }
